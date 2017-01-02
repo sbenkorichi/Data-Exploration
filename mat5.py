955b4db755d64958fddb945174883601ea581eb8
Salah Benkorichi <salah.benkorichi@hotmail.com>
2017-01-02 17:18:33 +0000

    Python Matplotlib Tutorial-5- How to read excel file part1&2

---
+++ mat5.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt 
+import pandas as pd
+
+# set directory
+df =   pd.read_excel('file1.xlsx', 'Sheet1' )
+df1 =  pd.read_excel('file1.xlsx', 'Sheet2' )
+df2 =  pd.read_excel('file2.xlsx', 'data' )
+
+# set plot
+plt.plot(df['time'], df['person1'], ls='--')
+plt.plot(df['time'], df1['person2'], ls=':')
+plt.plot(df['time'], df2['person3'])
+
+# set label
+plt.xlabel('time(days)')
+plt.ylabel('project')
+plt.title('Finished projects')
+plt.legend()
+
+plt.show()
